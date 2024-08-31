fb7d40814d113c5f00044babb074c4fa45e9eb7a
ToWelie89 <sonesson8909@hotmail.com>
2024-08-31 02:14:36 +0200

    more stuff

--- main.js
+++ main.js
@@ -5,8 +5,23 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const path = require("path");
+const fs = require("fs");
+const sharp = require('sharp');
 
 const PDFExtract = require("pdf.js-extract").PDFExtract;
+
+const Tesseract = require("tesseract.js");
+
+/* Tesseract.recognize('./pic.webp', "eng", {
+  logger: (m) => {
+    if (m.status === "recognizing text") {
+      console.log(m.progress);
+    }
+  },
+}).then(({ data: { text } }) => {
+  console.log('text')
+  console.log(text)
+}); */
 
 const PORT = process.env.PORT || 4000;
 
@@ -44,6 +59,38 @@ app.post("/upload_files", async (req, res) => {
       });
       console.log(all);
       res.send(all);
+    });
+  });
+});
+
+app.post("/upload_pic", async (req, res) => {
+  console.log("req.body", req.body);
+
+  let data = [];
+  req.on("data", (chunk) => {
+    data.push(chunk);
+  });
+
+  req.on("end", async () => {
+    let fileData = Buffer.concat(data);
+    console.log("total", fileData);
+
+    const buffer = await sharp(fileData)
+
+    console.log(buffer)
+
+    buffer.toFile('lol.png')
+
+    Tesseract.recognize('lol.png', "eng", {
+      logger: (m) => {
+        if (m.status === "recognizing text") {
+          console.log(m.progress);
+        }
+      },
+    }).then(({ data: { text } }) => {
+      console.log('text')
+      console.log(text)
+      res.send(text)
     });
   });
 });
